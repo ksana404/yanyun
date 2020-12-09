7e19d4754174a85a1190ca9bd7572396697dc721
ksana404 <ksana404@163.com>
2020-12-09 15:32:19 +0800

    IDEA创建dev-idea分支,新建删除.

---
+++ yy-web/src/main/java/com/yy/git/dev/idea/DevIDEA.java
@@ -0,0 +1,27 @@
+/**
+ * Copyright (C), 2015-2020, XXX有限公司
+ * FileName: DevIDEA
+ * Author:   Administrator
+ * Date:     2020/12/9 0009 15:28
+ * Description: dev-idea的分支创建新建类
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package com.yy.git.dev.idea;
+
+/**
+ * 〈一句话功能简述〉<br> 
+ * 〈dev-idea的分支创建新建类〉
+ *
+ * @author Administrator
+ * @create 2020/12/9 0009
+ * @since 1.0.0
+ */
+public class DevIDEA {
+    public static void main(String[] args) {
+        System.out.println("创建dev-idea的分支");
+        System.out.println("删掉GitDemo类");
+    }
+
+}
