75b7e84084fb890338040f00ed31bbedaf41da94
ksana404 <ksana404@163.com>
2021-01-16 20:18:19 +0800

    IDEA dev-idea分支:添加demo文件夹,Student学生类

---
+++ yy-web/src/main/java/com/yy/git/demo/Student.java
@@ -0,0 +1,17 @@
+package com.yy.git.demo;
+
+/**
+ * 〈一句话功能简述〉<br> 
+ * 〈测试〉
+ *
+ * @author  ksana404
+ * @create 2021/1/16 0016
+ * @since 1.0.0
+ */
+public class Student
+{
+    public static void main(String[] args) {
+        System.out.println("idea 提交");
+    }
+
+}
